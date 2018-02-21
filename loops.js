514b20d8ba060cbdc10f1b485fce4087706289de
Zachary Jones <zajonesck83@gmail.com>
2018-02-21 19:43:45 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -14,5 +14,11 @@ function whileLoop(n){
   return "done"
 }
 function doWhileLoop(array){
-  
+  function maybeTrue() {
+  return Math.random() >= 0.5
+}
+
+do {
+  console.log('doo-bee-doo-bee-doo')
+} while (maybeTrue());
 }
